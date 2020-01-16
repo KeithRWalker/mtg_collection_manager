8b9cbcfed174dbdbca57c87e1be482f3c2cc79f0
Keith Walker <Keithrwalker418@gmail.com>
2020-01-16 05:23:29 -0600

    setup react directory

---
+++ mtg-collection-manager.ui/src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
