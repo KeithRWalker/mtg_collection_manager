38e14f55703f81817f51ac0d9f37d1e2515db258
Keith Walker <Keithrwalker418@gmail.com>
2020-01-27 21:05:11 -0600

    fixed up nav

--- mtg-collection-manager.ui/src/components/MyNav/MyNav.js
+++ mtg-collection-manager.ui/src/components/MyNav/MyNav.js
@@ -24,9 +24,7 @@ class MyNav extends React.Component {
   state = {
     collapsed: true
   }
-  toggleNavbar = () => {
-    this.setState({ collapsed: !this.state.collapsed });
-  }
+  toggleNavbar = () => this.setState({ collapsed: !this.state.collapsed });
 
   logoutClickEvent = (e) => {
     e.preventDefault();
@@ -37,74 +35,78 @@ class MyNav extends React.Component {
   render() {
     const { collapsed } = this.state;
     const { authed } = this.props;
-
     let navItems;
-
-    if(authed){                             // LOGGED IN DROPDOWN
-      navItems =  <DropdownMenu right>
-              <DropdownItem >
-                <NavLink tag={RRNavLink} to="/home">Account</NavLink>
-              </DropdownItem>
-                <DropdownItem divider />
-              <DropdownItem>
-                <NavLink tag={RRNavLink} to="/landingPage" onClick={this.logoutClickEvent}>Logout</NavLink>
-              </DropdownItem>
-            </DropdownMenu>
+    if(authed){
+                  /*  LOGGED IN DROPDOWN  */
+      navItems =  <DropdownMenu left>
+                    <DropdownItem >
+                      <NavLink tag={RRNavLink} to="/home">Account</NavLink>
+                    </DropdownItem>
+                      <DropdownItem divider />
+                    <DropdownItem>
+                    <NavLink tag={RRNavLink} to="/landingPage" onClick={this.logoutClickEvent}>Logout</NavLink>
+                    </DropdownItem>
+                  </DropdownMenu>
     }
         
-    else{                                   // LOGGED OUT DROP DOWN
-      navItems =  <DropdownMenu right>
-              <DropdownItem>
-                <NavLink tag={RRNavLink} to="/login">Login</NavLink>
-              </DropdownItem>
-              <DropdownItem>
-                <NavLink tag={RRNavLink} to="/registerUser">Register</NavLink>
-              </DropdownItem>
-            </DropdownMenu>
+    else{
+                  /*  LOGGED OUT DROP DOWN   */
+      navItems =  <DropdownMenu left>
+                    <DropdownItem>
+                      <NavLink tag={RRNavLink} to="/login">Login</NavLink>
+                    </DropdownItem>
+                    <DropdownItem>
+                      <NavLink tag={RRNavLink} to="/registerUser">Register</NavLink>
+                    </DropdownItem>
+                  </DropdownMenu>
     }
 
     return (
       <div className="MyNav">
-        <Navbar color="dark" dark expand="lg">
-          <NavbarBrand href="/">MTG Binder</NavbarBrand>
-            <Nav navbar className="col-8">
-{
-                  // -- NAVIGATION BROWSE/DECKS  -- //  
-}
+        <Navbar className="row wrap" expand="lg">
+          
+            <Nav navbar className="col">
+            <NavbarBrand className="col-1" href="/">MTG Binder</NavbarBrand>
+            {/* --  NAVIGATION BROWSE/DECKS  -- */}
               <NavItem className="col-1">
                 <NavLink tag={RRNavLink} to="/browse/1">
                   Browse
                 </NavLink>
               </NavItem>
-
               <NavItem className="col-1">
-                <NavLink href="https://github.com/reactstrap/reactstrap">
+                <NavLink href="/decks">
                   Decks
                 </NavLink>
               </NavItem>
-{ 
-                  // -- SEARCH BAR  -- //  
-}
-              <NavItem className="col-12 mx-auto">
-                <InputGroup>
-                <Input />
-                  <InputGroupAddon addonType="append">
-                    <Button color="success">
+              <NavItem className="col-2">
+                <NavLink href="/user/collection">
+                  Your Collection
+                </NavLink>
+              </NavItem>
+
+              {/* --  SEARCH BAR  -- */}
+              <NavItem className="col-5">
+                <InputGroup className="">
+                <Input className ="search-bar"/>
+                  <InputGroupAddon className="" addonType="append">
+                    <Button className="">
                       Search
                     </Button>
                   </InputGroupAddon>
                 </InputGroup>
               </NavItem>
+              
+              <NavItem className="col-3">
+                <ButtonDropdown nav isOpen={!collapsed} toggle={this.toggleNavbar} className="">
+                  <DropdownToggle caret className="dropdown-account">
+                    Account
+                  </DropdownToggle>
+                  {navItems}
+                </ButtonDropdown>
+              </NavItem>
             </Nav>
-{ 
-                  // -- LOGIN/REGISTER/ACCOUNT DROPDOWN -- //  
-}
-            <Nav navbar className="col-1 ml-auto">
-              <ButtonDropdown nav isOpen={!collapsed} toggle={this.toggleNavbar} className="col-1 mx-auto">
-                <DropdownToggle caret color="info" className=""> Account </DropdownToggle>
-              {navItems}
-              </ButtonDropdown>
-            </Nav>
+
+            {/* --  LOGIN/REGISTER/ACCOUNT DROPDOWN  -- */}
         </Navbar>
       </div>
     );
